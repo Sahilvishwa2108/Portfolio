a55798df02f258c1b79dd6877202671028c62ed6
sahilvishwa2108 <sahilvishwa2108@gmail.com>
2024-12-05 22:44:45 +0530

    Refactor Hero component to improve layout and animation flow; add buttons for "Hire me" and "Resume" with enhanced visibility

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,57 +24,48 @@ function Hero() {
             duration: 0.8,
             ease: "easeInOut",
           }}
-          className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+          className="-mt-16 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
         >
-          <b>
-            <LetterPullup
-              words={"Hey there,"}
-              delay={0.02}
-              onComplete={() => setFirstLineComplete(true)}
-            />
-            {firstLineComplete && (
-            <LetterPullup
-              words={"I'm"} 
-              delay={0.02} 
-              onComplete={()=> setSecondLineComplete(true)} 
-            />
-            )}
-            {secondLineComplete && (
-            <LetterPullup
-              words={"Sahil Vishwakarma"} 
-              delay={0.05} 
-              onComplete={()=> setThirdLineComplete(true)} 
-            />
-            )}
+          <div className="h-80 pt-16">
+            <b>
+              <LetterPullup
+                words={"Hey there,"}
+                delay={0.02}
+                onComplete={() => setFirstLineComplete(true)}
+              />
+              {firstLineComplete && (
+                <LetterPullup
+                  words={"I'm"}
+                  delay={0.05}
+                  onComplete={() => setSecondLineComplete(true)}
+                />
+              )}
+              {secondLineComplete && (
+                <LetterPullup
+                  words={"Sahil Vishwakarma"}
+                  delay={0.05}
+                  onComplete={() => setThirdLineComplete(true)}
+                />
+              )}
             </b>
+            {thirdLineComplete && (
+              <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-8 justify-center items-center">
+                <Button
+                  borderRadius="1.75rem"
+                  className="bg-black border-neutral-600 text-white text-md"
+                >
+                  <BlurIn word="Hire me" />
+                </Button>
+                <Button
+                  borderRadius="1.75rem"
+                  className="bg-white text-black border-black  text-md"
+                >
+                  <BlurIn word="Resume" />
+                </Button>
+              </div>
+            )}
+          </div>
         </motion.h1>
-        {thirdLineComplete && (
-          <motion.h1
-            initial={{ opacity: 0.5, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{
-              delay: 0.3,
-              duration: 0.8,
-              ease: "easeInOut",
-            }}
-            className="mt-1 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
-          >
-            <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-8 justify-center items-center">
-              <Button
-                borderRadius="1.75rem"
-                className="bg-black border-neutral-600 text-white text-md"
-              >
-                <BlurIn word="Hire me" />
-              </Button>
-              <Button
-                borderRadius="1.75rem"
-                className="bg-white text-black border-black  text-md"
-              >
-                <BlurIn word="Resume" />
-              </Button>
-            </div>
-          </motion.h1>
-        )}
       </LampContainer>
       <GoogleGeminiEffectDemo />
     </>
